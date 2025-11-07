a8c77c9b2e327d065e1870983f6060f8058c392a
juan carlos nova <jcn1708@gmail.com>
2025-11-06 19:47:39 -0500

    fix: Correccion chart description prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,6 +1,6 @@
 import random
 
-DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no"'
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 MIN_NUMBER = 1
 MAX_NUMBER = 100
 
